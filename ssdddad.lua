85a00cd31eec35ad9b029066fa5d15583dbdd126
Vacumic <vvsdas299@gmail.com>
2024-10-22 13:05:51 +0300

    Update ssdddad.lua

--- ssdddad.lua
+++ ssdddad.lua
@@ -1,4 +1,4 @@
-wait (3)
+wait (5)
 _b.Size=UDim2.new(0,370,0,21)_b.Font=Enum.Font.Arial;_b.Text="https://www.youtube.com/@gaserxxx"
 _b.TextColor3=Color3.new(1.000000, 0.000000, 0.000000)_b.TextSize=20;ab.Parent=da
 wait (3)
